e642dcd1af85ce9e0fc08768de88eaad87506aa4
Sergei <1161441@gmail.com>
2019-11-04 11:28:30 +0200

    Initial installation

---
+++ migrations/m191104_092427_create_list_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%list}}`.
+ */
+class m191104_092427_create_list_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%list}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(),
+            'description' => $this->text()
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%list}}');
+    }
+}
